dc9e29cf4658b168eab7a9139633eab338d11b9a
sarathcj29 <sarathcj29@gmail.com>
2022-04-30 14:29:31 +0530

    Modified game.js to add core functionalities

--- game.js
+++ game.js
@@ -1,24 +1,62 @@
 // Variables
 var buttonColors = ['red', 'blue', 'green', 'yellow'];
+var randomNumber;
+var randomChosenColor;
 var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
 
 // Logic
-function nextRndmColor() {
-  return Math.floor(Math.random() * 4);
+function playSound(color) {
+  var colorAudio = new Audio(`sounds/${color}.mp3`);
+  colorAudio.play();
 }
 
-function playSound(soundColor) {
-  var sound = new Audio(`sounds/${soundColor}.mp3`);
-  sound.play();
+function checkAnswer(index) {
+  console.log(gamePattern, userClickedPattern);
+  if (gamePattern[index] === userClickedPattern[index]) {
+    userClickedPattern = [];
+    nextSequence();
+  } else {
+    playSound('wrong');
+    $('body').addClass('game-over');
+    setTimeout(() => {
+      $('body').removeClass('game-over');
+    }, 200);
+    $('h1').text(`Game Over, Press any key to Restart`);
+  }
 }
 
-function gameStart() {
-  $(`#level-title`).text('Level 1');
-  var randomChosenColor = buttonColors[nextRndmColor()];
-  playSound(randomChosenColor);
+function nextSequence() {
+  level++;
+  $('h1').text(`Level ${level}`);
+  randomNumber = Math.floor(Math.random() * 4);
+  randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
   $(`#${randomChosenColor}`).fadeOut(150).fadeIn(150);
+  playSound(randomChosenColor);
 }
 
-$(document).keypress(function (event) {
-  gameStart();
+function restartGame() {}
+
+function animatePress(color) {
+  $(`#${color}`).addClass('pressed');
+  setTimeout(() => {
+    $(`#${color}`).removeClass('pressed');
+  }, 100);
+}
+
+$('.btn').click(function (event) {
+  var userChosenColor = event.target.id;
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+  if (gamePattern.length == userClickedPattern.length)
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+$(document).keydown(function (event) {
+  console.log(event);
+  if (level == 0) nextSequence();
+  else restartGame();
 });
